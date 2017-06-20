43c622964700d20d8b3bc4768a897675cb5b74a4
Stan van Zanten <stanzanten@hotmail.com>
2017-06-20 09:57:48 +0200

    Register aangepast

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -27,7 +27,7 @@ routes.get('/goodbye', function(req, res){
 	res.json({ 'tekst': 'Goodbye!'});
 });
 
-routes.get('register', function(req,res){
+routes.get('/register', function(req,res){
     res.contentType(application);
     res.status(200);
     res.json({'tekst': 'Hallo Nino!'});
